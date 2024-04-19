efd0f54941c08fbd03debcadfaf68652103a2467
The Server Guy <sebastian.schmidt@uni-kassel.de>
2024-04-19 13:35:31 +0200

    update

---
+++ superSecret.py
@@ -0,0 +1,5 @@
+kenya_client_id = 'sh-7fe8180b-4e41-40fd-ad8e-f24ca03da71a'
+secret = 'nLAQY3RBwsFJCgTI60OC2xcrYj4SVmKu'
+
+jupyter_client_id = 'sh-f409cb6a-725d-469c-a88d-159bfee1f813'
+jupyter_secret = 'en7cPBKZ81BAUzofUekgivFBbtpRCCRD'
